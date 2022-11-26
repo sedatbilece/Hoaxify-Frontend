f755e0b5a367f0fafe91cd6f189e3740567bd964
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-11-26 14:35:33 +0300

    add Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './style/App.css';
 import UserSignupPage from './pages/UserSignupPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       
       <UserSignupPage />
+      <hr></hr>
+
+      <LoginPage />
     </div>
   );
 }

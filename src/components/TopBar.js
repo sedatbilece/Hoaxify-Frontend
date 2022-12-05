6e85171f33fd7c58425899e89024bf28c79985d6
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-12-05 13:58:45 +0300

    add(topbar) : switch islogged

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,7 +1,38 @@
 import React from "react";
 import logo from "../assets/hoaxify.png"
 import {Link, HashRouter as Router,} from "react-router-dom";
+import { useState } from "react";
+
 const TopBar = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState('user1');
+
+    let links =(
+        <>
+                
+                <li>
+                <Link to="/login" className="text-gray-900 dark:text-white hover:underline">Login</Link> 
+                </li>
+                <li>
+                <Link to="/signup" className="text-gray-900 dark:text-white hover:underline">SignUp</Link> 
+                </li>  
+                </>
+    )
+        let links2 = (
+            <>
+                    <li>
+                    <Link to={"/user/"+username} className="text-gray-900 dark:text-white hover:underline">
+                        {username}
+                        </Link> 
+                    </li>
+                    <li>
+                    <Link to="/logout" className="text-gray-900 dark:text-white hover:underline">Logout</Link> 
+                    </li>
+                     
+                    </>
+        );
+    
     return (
        <div>
        
@@ -14,18 +45,11 @@ const TopBar = () => {
         </Link>
         
         <div class="flex items-center">
-            <a href="#" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline mr-6">xxx</a>
+            
             <ul class="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
-                <li>
-                <Link to="/user" className="text-gray-900 dark:text-white hover:underline">User</Link> 
-                </li>
-                <li>
-                <Link to="/login" className="text-gray-900 dark:text-white hover:underline">Login</Link> 
-                </li>
-                <li>
-                <Link to="/signup" className="text-gray-900 dark:text-white hover:underline">SignUp</Link> 
-                </li>  
+                
+                
+                {!isLoggedIn  ? links : links2}
                 
             </ul>
         </div>
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserPage = () => {
+    let { id } = useParams();
     return (
         <div>
         <h1>User Page</h1>
+
         </div>
     );
     }

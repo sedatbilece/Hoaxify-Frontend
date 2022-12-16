6e7a18249d4a6c7c291f4559e1fd9219b273fbf9
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-12-16 17:56:57 +0300

    add(UserPage) error handling

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useLocation   } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getUser } from "../api/apiCalls";
 const UserPage = () => {
     let { username } = useParams();
+    let location = useLocation();
 
     const [user, setUser] = useState({});
-
+    const [error, setError] = useState(null);
+    const[notFound,setNotFound] = useState(false);
     useEffect(() => {
         loadUser(username);
     }, []);
 
+    useEffect(() => {
+        loadUser(username);
+     },[location]) 
+
     const loadUser = (username) => {
         getUser(username).then((response) => {
             setUser(response.data);
+            setNotFound(false);
             console.log("user:", response.data);
         }).catch((error) => {
-            console.log(error)
+            console.log("error:", error);
+            setNotFound(true);
+            setError(error.response.data.message);
         }
         );
     }
@@ -25,7 +34,9 @@ const UserPage = () => {
     return (
         <div>
         <h1>User Page</h1>
-        <p>Id: {username}</p>
+        <hr></hr>
+        
+        {notFound ? <p className="bg-red-100 border  text-red-700 px-4 py-3 rounded relative m-1">{error}</p>:<p >Username: {user.username}</p>}
         </div>
     );
     }

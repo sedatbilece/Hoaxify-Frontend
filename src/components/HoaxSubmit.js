e1a5802433dc753b02d984b12724fdbad6414c5a
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-12-25 17:15:04 +0300

    fix HoaxSubmit design

--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "../style/HoaxSubmit.css"
-
+import { useState } from "react";
 const HoaxSubmit = (props) => {
+
+       const [focused, setFocused] = useState(false);
+       const [hoax,setHoax] = useState(''); 
     return (
         <div className="conx">
             
             <div className="wrapper "> 
                   <form className="list card-shadow ">
-                    <textarea className="hoaxText " placeholder="What's on your mind?" />
-                    <button className="hoaxBtn shadow bg-cyan-300 hover:bg-cyan-400 focus:shadow-outline 
-      focus:outline-none  text-white font-bold py-2 px-4 rounded">Hoax</button>
+                    <textarea rows={focused ? "5":"2"}  
+                    onFocus={()=>setFocused(true)} 
+                    className="hoaxText " placeholder="What's on your mind?"  />
+                    {focused && (
+                        <div className="button-wrap">
+                        <button className=" bg-cyan-400 hover:bg-cyan-500 focus:shadow-outline 
+                        focus:outline-none  text-white ml-2 font-bold py-2 px-4 rounded">Hoax</button>
+                        <button  type ="reset"  className="bg-red-400 hover:bg-red-500 focus:shadow-outline 
+                        focus:outline-none  text-white  ml-2  font-bold py-2 px-3 rounded" 
+                            >X</button>
+                            </div>
+                    )}
                   </form>
             </div>
 
